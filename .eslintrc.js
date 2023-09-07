e5be5226ef9300d1bca8748b269f6df79ff6eb22
lenghak.dev <lenghakhok.dev@gmail.com>
2023-09-07 16:59:36 +0700

    [.eslintrs.js] ~ fixed prettier eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   ignorePatterns: [".eslintrc.js"],
   rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
@@ -38,5 +39,4 @@ module.exports = {
       },
     ],
   },
-  "prettier/prettier": ["error", { endOfLine: "auto" }],
 };
