62279aaed265fc73ab5458b5b4c1be4097783d7c
lenghak.dev <lenghakhok.dev@gmail.com>
2023-09-08 09:35:45 +0700

    [middleware] removed log

--- src/middleware/app.middleware.ts
+++ src/middleware/app.middleware.ts
@@ -4,7 +4,6 @@ import type { Request, Response } from "express";
 
 export class AppMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: (error?: any) => void) {
-    console.log(req.body, res);
     next();
   }
 }
