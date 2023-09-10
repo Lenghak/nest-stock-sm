211d105de48be17cb10d808dcd15a6c1bd69bbdd
lenghak.dev <lenghakhok.dev@gmail.com>
2023-09-10 13:33:37 +0700

    [users\controller] ~ added try and catch

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Patch,
   Post,
@@ -10,9 +11,10 @@ import {
   Res,
 } from "@nestjs/common";
 
+import { Prisma } from "@prisma/client";
+
 import { Request, Response } from "express";
 
-import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { UsersService } from "./users.service";
 
@@ -21,27 +23,74 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create(userCreateInput: Prisma.UserCreateInput) {
+    return this.usersService.create(userCreateInput);
   }
 
   @Get()
-  findAll(@Req() request: Request, @Res() response: Response) {
-    return this.usersService.findAll();
+  async findAll(@Req() _req: Request, @Res() res: Response) {
+    try {
+      const user = await this.usersService.findAll();
+
+      return res.status(HttpStatus.OK).json({
+        data: user,
+      });
+    } catch (err) {
+      throw err;
+    }
   }
 
   @Get(":id")
-  findOne(@Req() req: Request, @Res() res: Response, @Param("id") id: string) {
-    return this.usersService.findOne(id);
+  async findOne(
+    @Req() _req: Request,
+    @Res() res: Response,
+    @Param("id") id: string,
+  ) {
+    try {
+      const user = await this.usersService.findOne(id);
+
+      return res.status(HttpStatus.OK).json({
+        data: user,
+      });
+    } catch (err) {
+      throw err;
+    }
   }
 
   @Patch(":id")
-  update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  async update(
+    @Req() _req: Request,
+    @Res() res: Response,
+    @Param("id") id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    try {
+      const updatedUser = await this.usersService.update(id, updateUserDto);
+
+      return res.status(HttpStatus.OK).json({
+        data: updatedUser,
+        message: "User updated successfully",
+      });
+    } catch (err) {
+      throw err;
+    }
   }
 
   @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.usersService.remove(+id);
+  async remove(
+    @Req() _req: Request,
+    @Res() res: Response,
+    @Param("id") id: string,
+  ) {
+    try {
+      const deletedUser = await this.usersService.remove(id);
+
+      return res.status(HttpStatus.OK).json({
+        data: deletedUser,
+        message: "User deleted successfully",
+      });
+    } catch (err) {
+      throw err;
+    }
   }
 }
