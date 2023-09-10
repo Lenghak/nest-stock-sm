b7f39ff2bad0e55d0ae329c5870f4ee82eda7f4c
lenghak.dev <lenghakhok.dev@gmail.com>
2023-09-10 18:10:17 +0700

    [app] ~ added products

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import {
 } from "@nestjs/common";
 
 import { AppMiddleware } from "./middleware/app.middleware";
+import { ProductsModule } from "./products/products.module";
 import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ProductsModule],
   controllers: [],
   providers: [],
 })
