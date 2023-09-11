0e2569d0ca07f1a7667ef7f523c265eb58696f80
lenghak.dev <lenghakhok.dev@gmail.com>
2023-09-11 12:59:59 +0700

    [models\products\dto] ~ added more validation

--- src/models/products/dto/create-product.dto.ts
+++ src/models/products/dto/create-product.dto.ts
@@ -1,26 +1,35 @@
 import { type Prisma } from "@prisma/client";
 
-import { IsCurrency, IsString } from "class-validator";
+import { IsCurrency, IsEmpty, IsString, Max } from "class-validator";
 
 export class CreateProductDto implements Prisma.ProductsCreateInput {
+  @IsEmpty()
+  productId?: string;
+
   @IsString()
+  @Max(256)
   productCode: string;
 
   @IsString()
+  @Max(256)
   productName: string;
 
   @IsString()
+  @Max(2048)
   productDescription: string;
 
   @IsString()
+  @Max(256)
   productType: string;
 
   @IsCurrency()
   productPrice: string;
 
   @IsString()
+  @Max(256)
   productCategory: string;
 
   @IsString()
+  @Max(256)
   barCode: string;
 }
